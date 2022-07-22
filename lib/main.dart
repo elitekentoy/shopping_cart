13acc91c07c48ff1b1027cb2170811ac9204bdf8
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-07-22 12:37:15 +0800

    Adjust import to current HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
-import 'package:provider_test/home_page.dart';
+import 'home_page.dart';
 
 import 'data_class.dart';
 
