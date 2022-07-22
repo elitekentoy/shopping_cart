b8c479cf513f2d315271c4efc704fa4b11043f0a
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-07-22 12:43:28 +0800

    Remove Unused Imports

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:provider/provider.dart';
 // import 'package:provider_test/second_page.dart';
 
